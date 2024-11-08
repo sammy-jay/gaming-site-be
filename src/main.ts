0b6b06d0267c82bf37d67b28b9f2d98df2d8dac0
Tolu Soneye <sammyjayden27@gmail.com>
2024-11-08 18:43:01 +0000

    added vercel.json

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1/docs', app, document);
+  SwaggerModule.setup('c', app, document);
 
   await app.listen(3001);
 }
